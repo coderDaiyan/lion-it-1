036e50d440894ff8e54987c485f3493bb40fc670
coderDaiyan <daiyanabdallah@gmail.com>
2021-05-30 21:39:31 +0600

    test

--- src/components/Home/HeaderMain/HeaderMain.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import HeaderMain from "./HeaderMain";
-
-test("should render headerMain component", () => {
-  render(<HeaderMain />);
-  const headerMainElement = screen.getByTestId("headerMain-1");
-  expect(headerMainElement).toBeInTheDocument();
-  expect(headerMainElement).toHaveTextContent("experienced");
-});
